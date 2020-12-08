041f8bf3edf0c2f7190143f0a29691e67887d342
Gerald Bivens <geraldbivens@gmail.com>
2020-12-08 05:28:53 -0700

    Cleaned up the code on HTML and CSS pages and added a resources page

--- frontend-talking-code-app/mod-1.js
+++ frontend-talking-code-app/mod-1.js
@@ -1,36 +1,6 @@
 const baseURL = 'http://localhost:3000/'
 const questionURL = `${baseURL}` + 'questions/'
 
-// let $questionContainer = document.querySelector('#featured-questions')
-
-// fetch(questionURL)
-//     .then(response => response.json())
-//     .then(result => {
-//         result.questions.map( question => {
-//             console.log(question)
-//             const $questionText = document.createElement('li')
-
-//             $questionText.innerHTML = `<a href='question.html?id=${question.id}'>${question.text}</a>`
-
-//             $questionContainer.append($questionText)
-//         })
-//     })
-
-// const questionContainer = document.querySelector('#content')
-
-// fetch(questionURL)
-//     .then(response => response.json())
-//     .then(result => {
-//         result.questions.map( question => {
-//             console.log(question)
-//             const questionText = document.createElement('div')
-
-//             questionText.innerHTML = `<a href='question.html?id=${question.id}'>${question.text}</a>`
-
-//             questionContainer.append(questionText)
-//         })
-//     })
-
 const questionContainer = document.querySelector('#content')
 
 fetch(questionURL)
